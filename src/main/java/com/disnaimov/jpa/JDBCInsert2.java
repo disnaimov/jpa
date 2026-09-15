de570d9318953131715ca5699186ca4e5cd0f531
disnaimov <dimitriy.kiselev0@mail.ru>
2026-09-15 17:32:15 +0400

    [feat] Добавлен альтерантивный вариант добавления

---
+++ src/main/java/com/disnaimov/jpa/JDBCInsert2.java
@@ -0,0 +1,34 @@
+package com.disnaimov.jpa;
+
+import java.sql.*;
+
+public class JDBCInsert2 {
+    static final String DB_URL = System.getenv("DB_URL");
+    static final String USER = System.getenv("DB_USER");
+    static final String PWD = System.getenv("DB_PASSWORD");
+
+    public static void main(String[] args) {
+        Connection connection = null;
+        Student student = new Student("Leo", "Farrell", 8.4);
+
+        try {
+            connection = DriverManager.getConnection(DB_URL, USER, PWD);
+            Statement statement = connection.createStatement();
+            String sqlQuery = "insert into students(name, surname, avg_grade) values" + "('"
+                    + student.getName() + "', '" + student.getSecondName() + "', " + student.getAvgGrade() + ")" ;
+
+            statement.executeUpdate(sqlQuery);
+
+            statement.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
